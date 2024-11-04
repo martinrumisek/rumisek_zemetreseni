d7f79bacee97077e9c6e30c8c3eb55cdaf530dda
DavidCitron <zizlavsky.d@seznam.cz>
2024-11-04 11:17:29 +0100

    Zprovoznění hlavní stránky

---
+++ app/Views/skibidi.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?=base_url('node_modules/leaflet/dist/leaflet.css');?>">
+    <script src="<?=base_url('node_modules/leaflet/dist/leaflet.js');?>"
+    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+    crossorigin=""></script>
+    <style>#map { height: 580px; width: 580px}</style>
+</head>
+<body>
+    <div id="map"></div>
+    <script>
+        var map = L.map('map').setView([51.505, -0.09], 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    </script>
+</body>
+</html>
