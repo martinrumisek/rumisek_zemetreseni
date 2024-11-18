cdf817ff4afac008a0e72d0146d89b864648a62b
martinrumisek <mrumisek@seznam.cz>
2024-11-18 10:04:44 +0100

    Změna souřadnic na ČR.

--- app/Views/skibidi.php
+++ app/Views/skibidi.php
@@ -19,7 +19,7 @@
 <body>
     <div id="map"></div>
     <script>
-        var map = L.map('map').setView([51.505, -0.09], 13);
+        var map = L.map('map').setView([49.8175, 15.473], 4); // Střed ČR, zoom 4
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
